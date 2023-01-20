60a3839d77b579dcf3cf118495e70dd006ee2f16
Elias notebook <eliasbragga44@gmail.com>
2023-01-19 22:53:20 -0300

    deleting folders

--- src/components/SeuNome.js
+++
@@ -1,9 +0,0 @@
-function SeuNome({setNomeS}) {
-    return (
-        <>
-            <input type="text" onChange={(e) => setNomeS(e.target.value)}/>
-        </>
-    )
-}
-
-export default SeuNome
